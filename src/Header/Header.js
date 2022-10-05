b237e3b63cb5288f17c5191f14c3b6108db07cd3
Anucet Murhula <anicetmurhula20@gmail.com>
2022-10-05 08:55:23 +0200

    Update files and fix linters issues

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,49 +1,80 @@
-import React from "react";
-import "./header.css";
+import React, { useState } from 'react';
+import './header.css';
 
 export default function Header() {
-  return (
-    <div className="main-container">
-      <div className="headerCardreverse">
-        <div className="header">
-          <h1>
-            My <br /> <span className="headerSpan">Event</span> <br /> Reminder
-          </h1>
-          <div className="description">
-            Easily host and share events with your friends across any social
-            media.
-          </div>
-        </div>
+  const [like, setLikes] = useState(0);
 
-        <div className="eventCard">
-          <div className="evemtImg">
-            <img
-              src="https://i.pinimg.com/236x/f4/64/19/f464193aada43dc0f7ebc334dc6570c2.jpg"
-              alt="movie event img"
-              className="evemtImg"
-            />
+  return (
+    <>
+      <div className="main-container">
+        <div className="headerCardreverse">
+          <div className="header">
+            <h1>
+              My
+              {' '}
+              <br />
+              {' '}
+              <span className="headerSpan">Event</span>
+              {' '}
+              <br />
+              {' '}
+              Reminder
+            </h1>
+            <div className="description">
+              Easily host and share events with your friends across any social
+              media.
+            </div>
           </div>
-          <div className="eventCardContenr">
-            <h2 className="eventName">Night Movie</h2>
-            <div className="hostName">
-              <ion-icon name="person"></ion-icon> Hosted by: <span>Evans</span>
+
+          <div className="eventCard">
+            <div className="evemtImg">
+              <img
+                src="https://i.pinimg.com/236x/f4/64/19/f464193aada43dc0f7ebc334dc6570c2.jpg"
+                alt="movie event img"
+                className="evemtImg"
+              />
             </div>
-            <div className="eventDetails">
-              <div className="startTime">
-                <ion-icon name="alarm"></ion-icon>start time:{" "}
+            <div className="eventCardContenr">
+              <h2 className="eventName">Night Movie</h2>
+              <div className="hostName">
+                <ion-icon name="person" />
+                {' '}
+                Hosted by:
+                {' '}
+                <span>Evans</span>
               </div>
-              <div className="endTime">
-                <ion-icon name="stopwatch"></ion-icon>end time:{" "}
+              <div className="eventDetails">
+                <div className="startTime">
+                  <ion-icon name="alarm" />
+                  start time:
+                  {' '}
+                </div>
+                <div className="endTime">
+                  <ion-icon name="stopwatch" />
+                  end time:
+                  {' '}
+                </div>
+                <div className="eventLocation">
+                  <ion-icon name="pin" />
+                  {' '}
+                  Location:
+                  {' '}
+                </div>
               </div>
-              <div className="eventLocation">
-                <ion-icon name="pin"></ion-icon> Location:{" "}
+              <div className='likesContainer'>
+                <button type="button" className="likes" onClick={() => setLikes(like + 1)}>
+                    {' '}
+                    <ion-icon name="happy" />
+                    {' '}
+                </button>
+                <div>{like}</div>
               </div>
+              
             </div>
-            <button className="likes"><ion-icon name="happy"></ion-icon></button>
           </div>
         </div>
+        <button type="button" className="createButton">Create my event</button>
       </div>
-      <button className="createButton">Create my event</button>
-    </div>
+    </>
   );
 }
